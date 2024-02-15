630ab6678369bc09037efed3063ea65cf8807ad7
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-02-15 22:12:03 +0200

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './scss/app.scss';
-
+import './App.css'
+import Header from './Header/Header';
 
 function App() {
 
 
   return (
     <>
-     a
+     <Header/>
+     
     </>
   );
 }
---
+++ src/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <div className='container'>
+                <div className='header'>
+                    HEADER
+                </div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
