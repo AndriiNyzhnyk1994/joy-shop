615cd24926cd6a09919bc1da3f2096be9684ef7c
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-02-15 19:59:04 +0200

    added some libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      Joy Shop
-    </div>
+    <>
+     a
+    </>
   );
 }
 
