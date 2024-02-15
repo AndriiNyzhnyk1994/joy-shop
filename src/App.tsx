628232d251077fa8faa2fc41314a63f6f4e14cd5
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-02-15 23:28:31 +0200

    added defaul reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,18 @@ function App() {
   return (
     <>
      <Header/>
-     
+     <main>
+      <div className='container'>
+        <div className='items'>
+          <ul className='itemsList'>
+            <li className="item">
+              <img src="https://m.media-amazon.com/images/I/61dV+aYlNmL._SX679_.jpg" alt="IMAGE" />
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+     </main>
     </>
   );
 }
